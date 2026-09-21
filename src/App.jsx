de12630f09324d4e90ed4c153d0d23227ac4e127
Akanksha <akankshamothukuri49@example.com>
2026-09-21 10:55:25 +0530

    Added the Ingriends and Safe Deleteion of the stocks from stocks

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import stockData from "./data/stock.json";
 import recipesData from "./data/recipes.json";
 import StockList from "./components/StockList";
 import Menu from "./components/Menu";
+import AddIngredientForm from "./components/AddIngredientForm";
 import { deductIngredients } from "./utils/inventory";
+import { findRecipesUsingIngredient } from "./utils/ingredients";
 import "./App.css";
 
 function App() {
@@ -14,8 +16,18 @@ function App() {
     setStock(stock.map((item) => (item.id === updatedItem.id ? updatedItem : item)));
   }
 
+  function handleAdd(newItem) {
+    setStock([...stock, newItem]);
+  }
+
   function handleDelete(id) {
-    console.log("Delete clicked:", id);
+    const usedBy = findRecipesUsingIngredient(id, recipes);
+    if (usedBy.length > 0) {
+      const dishNames = usedBy.map((r) => r.name).join(", ");
+      alert(`Cannot delete this ingredient. It is used by: ${dishNames}`);
+      return;
+    }
+    setStock(stock.filter((item) => item.id !== id));
   }
 
   function handleOrder(dish) {
@@ -26,6 +38,7 @@ function App() {
   return (
     <div>
       <StockList stock={stock} onSave={handleSave} onDelete={handleDelete} />
+      <AddIngredientForm stock={stock} onAdd={handleAdd} />
       <Menu recipes={recipes} stock={stock} onOrder={handleOrder} />
     </div>
   );
